15286d1bab539bb1861f1b7d41ac122290fa517f
Harsh Virani <iamharshvirani7@gmail.com>
2024-03-19 22:31:08 +0530

    Added basic code to curl public ip

--- main.tf
+++ main.tf
@@ -6,7 +6,28 @@ resource "aws_instance" "example" {
   ami           = "ami-0b8b44ec9a8f90422"
   instance_type = "t3.micro"
 
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
+
    tags = {
     Name = "my-ubuntu"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "web"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
